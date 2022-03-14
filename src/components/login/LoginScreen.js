37ff5402e0007d8dc14ecb904787afc6423da7ef
Daniel Fernandez Guarneros <danierse63@gmail.com>
2022-03-13 20:07:13 -0600

    05 Navigate push / replace - useNavigate

--- src/components/login/LoginScreen.js
+++ src/components/login/LoginScreen.js
@@ -1,9 +1,27 @@
+import { useNavigate } from 'react-router-dom';
 
 
 export const LoginScreen = () => {
-    return (
-      <div>
-          <h1>LoginScreen</h1>
-      </div>
-    )
+
+  const navigate = useNavigate();
+
+  const handleLogin = () => {
+    navigate('/marvel', {
+      replace: true
+    });
   }
+
+  return (
+    <div >
+      <h1>Login</h1>
+      <hr />
+
+      <button
+        className='btn btn-primary'
+        onClick={ handleLogin }
+        >
+          Login
+      </button>
+    </div>
+  )
+}
