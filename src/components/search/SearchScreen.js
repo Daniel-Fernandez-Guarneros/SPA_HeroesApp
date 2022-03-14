eabb4619cd492e00ee31339ff9797a6c3f0ec93f
Daniel Fernandez Guarneros <danierse63@gmail.com>
2022-03-13 22:21:31 -0600

    13 Mostrar listado de héroes

--- src/components/search/SearchScreen.js
+++ src/components/search/SearchScreen.js
@@ -1,13 +1,16 @@
 import { useForm } from '../../hooks/useForm';
+import { getHeroesByName } from '../../selectors/getHeroesByName';
+import { HeroCard } from '../hero/HeroCard';
 
 
 export const SearchScreen = () => {
 
     const [ formValues, handleInputChange ] = useForm({
         searchText: '',
-    })
+    });
 
     const { searchText } = formValues;
+    const heroesFileted = getHeroesByName('Algo por aqui');
 
 
     const handleSearch = (e) => {
@@ -49,6 +52,24 @@ export const SearchScreen = () => {
 
 
                 </div>
+
+                <div className="col-7">
+                    <h4>Resultados</h4>
+                    <hr />
+
+
+                    {
+                        heroesFileted.map(hero => (
+                            <HeroCard 
+                                key={ hero.id }
+                                { ...hero }
+                            />
+                        ))
+                    }
+
+
+                </div>
+
             </div>
 
         </>
